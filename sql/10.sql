2be3a1842b8d6dd3751d26b45e8c3bfa0359138b
Kush <kushpatel@gmail.com>
2023-02-20 14:01:40 -0800

    problem 10 fix

--- sql/10.sql
+++ sql/10.sql
@@ -7,6 +7,6 @@ SELECT film.title, film.film_id, count(film_actor.actor_id) AS actor_count
 FROM film
 JOIN film_actor ON film_actor.film_id = film.film_id
 GROUP BY film.film_id
-ORDER BY actor_count, film_id ASC
+ORDER BY actor_count, film.title ASC
 
 
